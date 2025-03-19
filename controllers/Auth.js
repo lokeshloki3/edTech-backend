86fc9bdedcbcb4a2726b0e3f9463ee25669f88ec
lokeshloki3 <lokesh1992p@gmail.com>
2025-03-19 23:55:42 +0530

    OTP controller created

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,6 +1,48 @@
-
+const User = require("../models/User");
+const OTP = require("../models/OTP");
+const otpGenerator = require("otp-generator")
 
 // sendOTP
+exports.sendOTP = async (req, res) => {
+    try {
+        // fetch email from req body
+        const { email } = req.body;
+
+        // check if user already exist
+        const checkUserPresent = await User.findOne({ email });
+
+        // if user already exist, then return a response
+        if (checkUserPresent) {
+            return res.status(401).json({
+                success: false,
+                message: "User already registered",
+            })
+        }
+
+        // generate otp
+        var otp = otpGenerator.generate(6, {
+            upperCaseAlphabets: false,
+            lowerCaseAlphabets: false,
+            specialChars: false,
+        });
+        console.log("OTP generated", otp);
+
+        // check unique otp or not or we can use library which will auto give unique otp everytime
+        const result = await OTP.findOne({ otp: otp });
+
+        while (result) {
+            var otp = otpGenerator.generate(6, {
+                upperCaseAlphabets: false,
+                lowerCaseAlphabets: false,
+                specialChars: false,
+            });
+            result = await OTP.findOne({ otp: otp });
+        }
+        
+    } catch (error) {
+
+    }
+}
 
 // signUp
 
