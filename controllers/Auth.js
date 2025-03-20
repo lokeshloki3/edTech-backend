c825be2630c6412e0fb5fa6a8dd28b2938dedc83
lokeshloki3 <lokesh1992p@gmail.com>
2025-03-20 15:22:18 +0530

    signUp controller created

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const OTP = require("../models/OTP");
-const otpGenerator = require("otp-generator")
+const otpGenerator = require("otp-generator");
+const bcrypt = require("bcrypt");
 
 // sendOTP
 exports.sendOTP = async (req, res) => {
@@ -40,7 +41,7 @@ exports.sendOTP = async (req, res) => {
         }
 
         const otpPayload = { email, otp };
-        
+
         // create an entry in db for OTP
         const otpBody = await OTP.create(otpPayload);
         console.log(otpBody);
@@ -61,6 +62,100 @@ exports.sendOTP = async (req, res) => {
 }
 
 // signUp
+exports.singUp = async (req, res) => {
+
+    try {
+        // data fetch from req body
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            confirmPassword,
+            accountType,
+            contactNumber,
+            otp
+        } = req.body;
+
+        // validate data
+        if (!firstName || !lastName || !email || !password || !confirmPassword || !otp) {
+            return res.status(403).json({
+                success: false,
+                message: "All fields required",
+            })
+        }
+
+        // 2 password match
+        if (password !== confirmPassword) {
+            return res.status(400).json({
+                success: false,
+                message: "Password and Confirm Password does not match",
+            });
+        }
+
+        // check user already exist or not
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: "User is already registered"
+            });
+        }
+
+        // find most recent OTP stored for the user
+        const recentOtp = await OTP.find({ email }).sort({ createdAt: -1 }).limit(1);
+        console.log(recentOtp);
+        // validate OTP
+        if (recentOtp.length == 0) {
+            // otp not found
+            return res.status(400).json({
+                success: false,
+                message: "OTP not found"
+            })
+        } else if (otp !== recentOtp.otp) {
+            // invalid otp
+            return res.status(400).json({
+                success: false,
+                message: "OTP not matching",
+            });
+        }
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // entry create in db
+        const profileDetails = await Profile.create({
+            gender: null,
+            dateOfBirth: null,
+            about: null,
+            contactNumber: null,
+        })
+
+        const user = await User.create({
+            firstName,
+            lastName,
+            email,
+            contactNumber,
+            password: hashedPassword,
+            accountType,
+            additionalDetails: profileDetails._id,
+            image: `http://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`, // dice bear api
+        })
+
+        // return res
+        return res.status(200).json({
+            success: true,
+            message: "User is registered successfully",
+            user,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: true,
+            message: "User cannot be registered. Please try again",
+        });
+    }
+}
 
 // Login
 
