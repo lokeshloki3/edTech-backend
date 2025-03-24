f97124a8502ded8e4675aeb5ff713b33b06699c0
lokeshloki3 <lokesh1992p@gmail.com>
2025-03-24 17:57:40 +0530

    Course controller created

--- controllers/Course.js
+++ controllers/Course.js
@@ -1,4 +1,95 @@
 const Course = require("../models/Course");
 const Tag = require("../models/Tags");
 const User = require("../models/User");
-const 
+const { uploadImageToCloudinary } = require("../utils/imageUploader");
+
+exports.courseDetails = async (req, res) => {
+    try {
+        // fetch data
+        const { courseName, courseDescription, whatYouWillLearn, price, tag } = req.body; // tag is id in course schema
+
+        // get thumbnail
+        const thumbnail = req.files.thumbnailImage;
+
+        // validation
+        if (!courseName || courseDescription || whatYouWillLearn || !price || !tag || !thumbnail) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required",
+            });
+        }
+
+        // check for instructor by getting object id from db(other method) using user id of intsructor is there in req body used in middleware(other method)
+        const userId = req.user.id; // user.decode in user in middleware
+        const instructorDetails = await User.findById(userId);
+        console.log("Instructor Details: ", instructorDetails);
+
+        if (!instructorDetails) {
+            return res.status(404).json({
+                success: false,
+                message: "Instructor Details not found",
+            });
+        }
+
+        // check given tag is valid or not - get object id of tag , user id of tag in req body
+        const tagDetails = await Tag.findById(tag);
+        if (!tagDetails) {
+            return res.status(404).json({
+                success: false,
+                message: "Tag Details not found",
+            });
+        }
+
+        // upload Image to Cloudinary
+        const thumbnailImage = await uploadImageToCloudinary(thumbnail, process.env.FOLDER_NAME);
+
+        // create an entry for new Course
+        // in course - instructor id is stored so required that so db call above is used
+        const newCourse = await Course.create({
+            courseName,
+            courseDescription,
+            instructor: instructorDetails._id,
+            whatYouWillLearn,
+            price,
+            tag: tagDetails._id, // or req body also have this
+            thumbnail: thumbnailImage.secure_url,
+        });
+
+        // add the new course to the user schema of Instructor
+        await User.findByIdAndUpdate(
+            { _id: instructorDetails._id },
+            {
+                $push: {
+                    courses: newCourse._id,
+                }
+            },
+            { new: true },
+        );
+
+        // update the Tag schema also
+        await Tag.findByIdAndUpdate(
+            { _id: tagDetails._id },
+            {
+                $push: {
+                    courses: newCourse._id,
+                }
+            },
+            { new: true },
+        );
+
+        // return response
+        return res.status(200).json({
+            success: true,
+            message: "Course created successfully",
+            data: newCourse,
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Failed to created course",
+            error: error.message,
+        });
+    }
+}
