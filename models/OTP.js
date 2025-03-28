e367ffe8334abbe88ce685639dd971f35f36991c
lokeshloki3 <lokesh1992p@gmail.com>
2025-03-28 19:03:23 +0530

    email template added in OTP Schema

--- models/OTP.js
+++ models/OTP.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const mailSender = require('../utils/mailSender');
+const emailTemplate = require("../mail/templates/emailVerificationTemplate");
 
 const OTPSchema = new mongoose.Schema({
     email: {
@@ -19,9 +20,18 @@ const OTPSchema = new mongoose.Schema({
 
 // a function -> to send emails pre save middleware hook to be sent before DB entry of actual Data
 async function sendVerificationEmail(email, otp) {
+    // Create a transporter to send emails
+
+    // Define the email options
+
+    // Send the email
     try {
-        const mailResponse = await mailSender(email, "Verification OTP from edTech", otp);
-        console.log("Email sent successfully: ", mailResponse);
+        const mailResponse = await mailSender(
+            email,
+            "Verification OTP from edTech",
+            emailTemplate(otp)
+        );
+        console.log("Email sent successfully: ", mailResponse.response);
     } catch (error) {
         console.log("Error occured while sending mails: ", error);
         throw error;
@@ -29,9 +39,19 @@ async function sendVerificationEmail(email, otp) {
 }
 
 // OTP pre save middleware added before OTP exports as pre hook
+// Define a post-save hook to send email after the document has been saved
 OTPSchema.pre("save", async function (next) {
-    await sendVerificationEmail(this.email, this.otp);
+    console.log("New document saved to database");
+
+    // Only send an email when a new document is created
+    if (this.isNew) {
+        await sendVerificationEmail(this.email, this.otp);
+    }
     next(); // Move to next middleware
 })
 
 mongoose.exports = mongoose.model("OTP", OTPSchema);
+
+// const OTP = mongoose.model("OTP", OTPSchema);
+
+// module.exports = OTP;
