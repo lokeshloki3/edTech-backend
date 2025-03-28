977eef86d8fa1516b6cdcc8c71126d8282d3b5ae
lokeshloki3 <lokesh1992p@gmail.com>
2025-03-28 18:57:37 +0530

    changed tag from object to arr string and added category as object id in Course schema

--- models/Course.js
+++ models/Course.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const courseSchema = new mongoose.Schema({
+const coursesSchema = new mongoose.Schema({
     courseName: {
         type: String,
         trim: true,
@@ -38,8 +38,13 @@ const courseSchema = new mongoose.Schema({
         type: String,
     },
     tag: {
+        type: [String],
+        required: true,
+    },
+    category: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Tag"
+        ref: "Category",
+        // required: true,
     },
     studentsEnrolled: [
         {
@@ -47,7 +52,14 @@ const courseSchema = new mongoose.Schema({
             required: true,
             ref: "User",
         }
-    ]
+    ],
+    instructions: {
+        type: [String],
+    },
+    status: {
+        type: String,
+        enum: ["Draft", "Published"],
+    },
 });
 
-module.exports = mongoose.model("Course", courseSchema);
+module.exports = mongoose.model("Course", coursesSchema);
