9dbf0054b228dd4f84ee5f75fc02bdfa85c76947
lokeshloki3 <lokesh1992p@gmail.com>
2025-03-28 22:51:11 +0530

    SubSection schema updated

--- controllers/SubSection.js
+++ controllers/SubSection.js
@@ -27,14 +27,15 @@ exports.createSubSection = async (req, res) => {
             videoUrl: uploadDetails.secure_url,
         });
         // update section with this sub section ObjectId
-        const updatedSection = await Section.findByIdAndUpdate({ _id: sectionId },
+        const updatedSection = await Section.findByIdAndUpdate(
+            { _id: sectionId },
             {
                 $push: {
                     subSection: subSectionDetails._id,
                 }
             },
             { new: true }
-        );
+        ).populate("subSection");
         // log updated section above, after adding populate here to have subsection details in section instead of id
         // return response
         return res.status(200).json({
