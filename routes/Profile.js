9d2df8e189d010baca89e05b2764b731afbd3f72
lokeshloki3 <lokesh1992p@gmail.com>
2025-04-01 14:00:46 +0530

    Profile route created

--- routes/Profile.js
+++ routes/Profile.js
@@ -2,17 +2,16 @@ const express = require("express")
 const router = express.Router()
 const { auth } = require("../middlewares/auth")
 const {
-  deleteAccount,
   updateProfile,
+  deleteAccount,
   getAllUserDetails,
   updateDisplayPicture,
   getEnrolledCourses,
 } = require("../controllers/Profile")
 
-// ********************************************************************************************************
-//                                      Profile routes
-// ********************************************************************************************************
-// Delet User Account
+// Profile routes
+
+// Delete User Account
 router.delete("/deleteProfile", auth, deleteAccount)
 router.put("/updateProfile", auth, updateProfile)
 router.get("/getUserDetails", auth, getAllUserDetails)
